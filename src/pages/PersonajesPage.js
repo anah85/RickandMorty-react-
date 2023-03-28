634cc7feea325501917a45428666df6d996debf2
Ana Hartmann <ana.hartmann@protonmail.com>
2023-03-28 17:29:41 +0200

    cambios nombre

--- src/pages/PersonajesPage.js
+++ src/pages/PersonajesPage.js
@@ -8,11 +8,13 @@ const PersonajesPage = ({character}) => {
      {character.map((characters, index)=>(
     <div key={index} className="col mb-4">
     <div classname="card">
+    <h5 className="card-title">{characters.name}</h5>
     <img src={characters.image} alt=""/>
     <div className="card-body">
-    <h5 className="card-title">{characters.name}</h5>
-    <p>Location: {characters.location.name}</p>
     <p>Species: {characters.species}</p>
+    <p>Gender: {characters.gender}</p>
+    <p>Status: {characters.status}</p>
+    <p>Location: {characters.location.name}</p>
     
     </div>
     </div>
